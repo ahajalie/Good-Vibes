2b82769b6193fb2c0947757c06108530fae41e43
Alie Hajalie <ahajalie@umich.edu>
2015-11-15 15:11:55 -0500

    Voice commands, text to speech, and connection checks done

--- app/src/main/java/com/example/hajalie/goodvibes2/MainActivity.java
+++ app/src/main/java/com/example/hajalie/goodvibes2/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.hajalie.goodvibes2;
 
+import android.bluetooth.BluetoothAdapter;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.os.Vibrator;
 import android.speech.RecognizerIntent;
@@ -21,6 +24,7 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import java.net.InetAddress;
 import java.util.ArrayList;
 import java.util.Locale;
 
@@ -36,6 +40,10 @@ public class MainActivity extends AppCompatActivity {
     private EditText txtText;
     TextToSpeech t1;
     protected static final int RESULT_SPEECH = 1;
+    private SharedPreferences preferences;
+    private SharedPreferences.Editor editor;
+    private boolean noErrors; //determine what to say on startup
+    private BluetoothAdapter mBluetoothAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,7 +59,9 @@ public class MainActivity extends AppCompatActivity {
 //                        .setAction("Action", null).show();
 //            }
 //        });
-
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        preferences = getApplicationContext().getSharedPreferences("preferences", 0);
+        editor = preferences.edit();
         btnSpeak = (ImageButton) findViewById(R.id.btnSpeak);
         txtText = (EditText) findViewById(R.id.direction_input);
         t1=new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
@@ -59,9 +69,24 @@ public class MainActivity extends AppCompatActivity {
             public void onInit(int status) {
                 if(status != TextToSpeech.ERROR) {
                     t1.setLanguage(Locale.US);
+                    noErrors = true;
+                    checkBluetooth();
+                    if(!isNetworkConnected()) {
+                        noErrors = false;
+                        t1.speak("Please enable WI-FI or network connectivity and try again.", TextToSpeech.QUEUE_ADD, null);
+                    }
+                    if(!isGPSEnabled()) {
+                        noErrors = false;
+                        t1.speak("GPS is disabled. Please enable GPS.", TextToSpeech.QUEUE_ADD, null);
+                    }
+                    if(noErrors) {
+                        t1.speak("Please tap the screen and speak your destination. Ask for help for more details.", TextToSpeech.QUEUE_ADD, null);
+                    }
+
                 }
             }
         });
+
 
         btnSpeak.setOnClickListener(new View.OnClickListener() {
 
@@ -107,24 +132,99 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private boolean isNetworkConnected() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        return (cm.getActiveNetworkInfo() != null) && cm.getActiveNetworkInfo().isConnectedOrConnecting();
+    }
+
+    public boolean isInternetAvailable() {
+        try {
+            InetAddress ipAddr = InetAddress.getByName("google.com"); //You can replace it with your name
+
+            if (ipAddr.equals("")) {
+                return false;
+            } else {
+                return true;
+            }
+
+        } catch (Exception e) {
+            return false;
+        }
+
+    }
+
+    private boolean isGPSEnabled (){
+        LocationManager locationManager = (LocationManager)
+                getSystemService(Context.LOCATION_SERVICE);
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+    }
+
+    private void checkBluetooth() {
+        if (mBluetoothAdapter == null) {
+            // Device does not support Bluetooth
+            noErrors = false;
+            t1.speak("Your device does not support blue tooth,, please use another device", TextToSpeech.QUEUE_ADD, null);
+        } else {
+            if (!mBluetoothAdapter.isEnabled()) {
+                // Bluetooth is not enable :)
+                noErrors = false;
+                t1.speak("Blue tooth is not enabled. Please enable blue tooth", TextToSpeech.QUEUE_ADD, null);
+            }
+        }
+    }
 
     private void processVoice(String str) {
         String string = str.toLowerCase();
         string = string.replaceAll("[!?,]", "");
         String[] strings = string.split("\\s+");
-        if(strings.length >= 2 && strings[0].equals("interval")) {
-            String value = strings[1];
-            float interval = Float.parseFloat(value);
-            t1.speak("Interval set to " + value, TextToSpeech.QUEUE_FLUSH, null);
+        if(containsWord(str, "interval")) {
+            float interval = containsNumber(str);
+            if(interval >= 0) {
+                editor.putFloat("interval", interval);
+                t1.speak("Interval set to " + Float.toString(interval) + "seconds", TextToSpeech.QUEUE_FLUSH, null);
+            }
+            else {
+                t1.speak("Invalid interval value, please try again", TextToSpeech.QUEUE_FLUSH, null);
+            }
         }
-        else if(strings.length >= 2 && strings[0].equals("intensity")) {
-            String value = strings[1];
-            float intensity = Float.parseFloat(value);
-            t1.speak("Intensity set to " + value, TextToSpeech.QUEUE_FLUSH, null);
+        else if(containsWord(str, "intensity")) {
+            float intensity = containsNumber(str);
+            if(intensity >= 0) {
+                editor.putFloat("intensity", intensity);
+                t1.speak("Intensity set to " + Float.toString(intensity), TextToSpeech.QUEUE_FLUSH, null);
+            }
+            else {
+                t1.speak("Invalid intensity value, please try again", TextToSpeech.QUEUE_FLUSH, null);
+            }
+        }
+        else if(containsWord(str, "help")) {
+            t1.speak("Tap the screen and say your destination", TextToSpeech.QUEUE_FLUSH, null);
+            t1.speak("You can also change the intensity value of the motor or the interval time between vibrations in seconds", TextToSpeech.QUEUE_ADD, null);
+            t1.speak("Once the directions have loaded, you can tap the screen and say cancel to cancel directions", TextToSpeech.QUEUE_ADD, null);
+
         }
         else {
-            t1.speak("Your Destination is" + str + ". Is this correct?", TextToSpeech.QUEUE_FLUSH, null);
+            t1.speak("Your Destination is" + str, TextToSpeech.QUEUE_FLUSH, null);
         }
+    }
+
+    /*
+    * Checks if string statement contains a number and returns that number
+    * If it doesn't contain a number, returns -1.0*/
+    private float containsNumber(String str) {
+        String string = str.toLowerCase();
+        string = string.replaceAll("[!?,]", "");
+        String[] strings = string.split("\\s+");
+        for(int i = 0; i < strings.length; ++i) {
+            try {
+                Float val = Float.valueOf(strings[i]);
+                if (val != null)
+                    return val.floatValue();
+            } catch (NumberFormatException e) {
+                //do nothing
+            }
+        }
+        return -1.0f;
     }
 
     private boolean containsWord(String str, String word) {
